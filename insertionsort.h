cf1c497d68f90b97155e45ca3d2a6fe45487ca91
oliveira-leandro826 <oliveira.leandro826@gmail.com>
2020-12-19 23:33:56 -0300

    add insertionsort

---
+++ insertionsort.h
@@ -0,0 +1,20 @@
+int insertionSort(int lista[], int tamanho)  
+{  
+    int i, j, m, comparacoes=0;
+	  
+    for (i = 1; i < tamanho; i++) 
+    {  
+        m = lista[i];  
+        j = i - 1;  
+        
+        while (j >= 0 && lista[j] > m) 
+        {  
+            lista[j + 1] = lista[j];  
+            j = j - 1;
+			comparacoes++;  
+        }  
+        lista[j + 1] = m;  
+    } 
+
+	return comparacoes;	 
+}  
